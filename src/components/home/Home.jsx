0a0ca0c1377f836ee8c07c782290dae8636dfd81
HenriqueMLDEV <hmeloleal@gmail.com>
2023-10-12 21:09:23 -0300

    Colocando Logo na Home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import "./Home.css"
 import { Link } from 'react-router-dom'
 import PainelCentral from '../painel-central/PainelCentral'
+import LogoReact from '../assets/react-logo.png'
+
 
 const Home = () => {
   return (
     <div>
-     <PainelCentral>
-        <img src="" alt="Logo React" />
+      <PainelCentral>
+        <img id='logoHome'src={LogoReact} alt="LogoReact"/>
         <h1 id="titulo">React Quiz</h1>
-         <Link to="/pergunta/1"><button class="button-17"
+        <Link to="/pergunta/1"><button class="button-17"
                 id="btn-iniciar">Iniciar o React Quiz</button></Link>
      </PainelCentral>
     </div>
@@ -17,4 +19,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
