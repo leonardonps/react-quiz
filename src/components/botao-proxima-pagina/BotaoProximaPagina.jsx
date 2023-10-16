ada3b474f5055dd2602fade04a27fb5236c2ded8
Leonardonps <leonardonpsilva@gmail.com>
2023-10-16 07:54:07 -0300

    Adicionando estilo ao botão de resposta; mudando a forma que a as respostas estão sendo exibidas

--- src/components/botao-proxima-pagina/BotaoProximaPagina.jsx
+++ src/components/botao-proxima-pagina/BotaoProximaPagina.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './BotaoProximaPagina.css'
 
 const BotaoProximaPagina = (props) => {
+
+  let textoBotao;
+
+  if(props.proximaPagina === "/resultado") {
+    textoBotao = "Finalizar o quiz"
+  } else {
+    textoBotao = "Próxima Pergunta"
+  }
+
   return (
     <div>
-    <Link to={props.proximaPagina}><button disabled={!props.disabledParent}>Próxima Pergunta</button></Link>
+    <Link to={props.proximaPagina}><button className='button-proxima-pagina' disabled={!props.disabledParent}>{textoBotao}</button></Link>
     </div>
   )
 }
