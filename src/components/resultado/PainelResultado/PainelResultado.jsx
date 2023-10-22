a53d48815ad86d0d32aa7f17f6b006fd8bfbbb7b
HenriqueMLDEV <hmeloleal@gmail.com>
2023-10-22 19:57:45 -0300

    Implementando Tela Resultado

--- src/components/resultado/PainelResultado/PainelResultado.jsx
+++ src/components/resultado/PainelResultado/PainelResultado.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import LogoReact from "../../assets/react-logo.png";
+import ImgResult1 from "../../assets/1_eletron_aprendiz.png";
+import ImgResult2 from "../../assets/2_quimico_reactivo.png";
+import ImgResult3 from "../../assets/3_mestre_reacoes.png";
+import ImgResult4 from "../../assets/4_atomo_nuclear.png";
+import ImgResult5 from "../../assets/5_radioativo.png";
 import "./PainelResultado.css";
 
 const PainelResultado = (props) => {
   if (props.porcentagemPontuacao < 25) {
     return (
       <>
-        <img id="logoHome" src={LogoReact} alt="LogoReact" />
+        <img className="imgResult" src={ImgResult1} alt="ImgResult1" />
         <h1 className="titulo-resultado">O Elétron Aprendiz</h1>
       </>
     );
@@ -16,8 +20,36 @@ const PainelResultado = (props) => {
   ) {
     return (
       <>
-        <img id="logoHome" src={LogoReact} alt="LogoReact" />
+        <img className="imgResult" src={ImgResult2} alt="ImgResult2" />
         <h1 className="titulo-resultado">O Quimico Reactivo</h1>
+      </>
+    );
+  } else if (
+    props.porcentagemPontuacao >= 50 &&
+    props.porcentagemPontuacao <= 75
+  ) {
+    return (
+      <>
+        <img className="imgResult" src={ImgResult3} alt="ImgResult3" />
+        <h1 className="titulo-resultado">O Mestre das Reações</h1>
+      </>
+    );
+  } else if (
+    props.porcentagemPontuacao >= 75 &&
+    props.porcentagemPontuacao <= 99
+  ) {
+    return (
+      <>
+        <img className="imgResult" src={ImgResult4} alt="ImgResult4" />
+        <h1 className="titulo-resultado">O Átomo Nuclear</h1>
+      </>
+    );
+  } else if (props.porcentagemPontuacao == 100) {
+    return (
+      <>
+        <img className="imgResult" src={ImgResult5} alt="ImgResult5" />
+        <h1 className="titulo-resultado">Radioativo</h1>
+        
       </>
     );
   }
