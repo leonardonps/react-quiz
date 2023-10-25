393782ffbcb5147d57d29fb57e830c4b212286c4
HenriqueMLDEV <hmeloleal@gmail.com>
2023-10-24 21:13:43 -0300

    Fazendo o merge do main para a branch-henriqueml

--- src/components/resultado/PainelResultado/PainelResultado.jsx
+++ src/components/resultado/PainelResultado/PainelResultado.jsx
@@ -1,55 +1,84 @@
 import React from "react";
-import ImgResult1 from "../../assets/1_eletron_aprendiz.png";
-import ImgResult2 from "../../assets/2_quimico_reactivo.png";
-import ImgResult3 from "../../assets/3_mestre_reacoes.png";
-import ImgResult4 from "../../assets/4_atomo_nuclear.png";
-import ImgResult5 from "../../assets/5_radioativo.png";
+import ImgResult1 from "../../../../assets/eletron-aprendiz-1.png";
+import ImgResult2 from "../../../../assets/quimico-reactivo-2.png";
+import ImgResult3 from "../../../../assets/mestre-reacoes-3.png";
+import ImgResult4 from "../../../../assets/atomo-nuclear-4.png";
+import ImgResult5 from "../../../../assets/radioativo-5.png";
 import "./PainelResultado.css";
 
 const PainelResultado = (props) => {
-  if (props.porcentagemPontuacao < 25) {
+  const porcentagemPontuacao = (props.pontos / props.qtdPerguntas) * 100;
+
+  if (porcentagemPontuacao < 25) {
     return (
       <>
-        <img className="imgResult" src={ImgResult1} alt="ImgResult1" />
+        <img
+          className="img-resultado"
+          src={ImgResult1}
+          alt="Emblema Életron Aprendiz"
+        />
         <h1 className="titulo-resultado">O Elétron Aprendiz</h1>
+        <h2 className="mensagem-resultado">
+          {" "}
+          Um passo de cada vez, pequeno gafanhoto!{" "}
+        </h2>
       </>
     );
-  } else if (
-    props.porcentagemPontuacao >= 25 &&
-    props.porcentagemPontuacao <= 50
-  ) {
+  } else if (porcentagemPontuacao >= 25 && porcentagemPontuacao < 50) {
     return (
       <>
-        <img className="imgResult" src={ImgResult2} alt="ImgResult2" />
-        <h1 className="titulo-resultado">O Quimico Reactivo</h1>
+        <img
+          className="img-resultado"
+          src={ImgResult2}
+          alt="Emblema Químico Reactivo"
+        />
+        <h1 className="titulo-resultado">O Químico Reactivo</h1>
+        <h2 className="mensagem-resultado">
+          {" "}
+          Continue a nadar... Continue a nadar... Você está progredindo!{" "}
+        </h2>
       </>
     );
-  } else if (
-    props.porcentagemPontuacao >= 50 &&
-    props.porcentagemPontuacao <= 75
-  ) {
+  } else if (porcentagemPontuacao >= 50 && porcentagemPontuacao < 75) {
     return (
       <>
-        <img className="imgResult" src={ImgResult3} alt="ImgResult3" />
+        <img
+          className="img-resultado"
+          src={ImgResult3}
+          alt="Emblema Mestre das Reações"
+        />
         <h1 className="titulo-resultado">O Mestre das Reações</h1>
+        <h2 className="mensagem-resultado">
+          {" "}
+          Parabéns! Um novo mestre do universo Front-End está a caminho...{" "}
+        </h2>
       </>
     );
-  } else if (
-    props.porcentagemPontuacao >= 75 &&
-    props.porcentagemPontuacao <= 99
-  ) {
+  } else if (porcentagemPontuacao >= 75 && porcentagemPontuacao < 100) {
     return (
       <>
-        <img className="imgResult" src={ImgResult4} alt="ImgResult4" />
+        <img
+          className="img-resultado"
+          src={ImgResult4}
+          alt="Emblema Átomo Nuclear"
+        />
         <h1 className="titulo-resultado">O Átomo Nuclear</h1>
+        <h2 className="mensagem-resultado">
+          {" "}
+          Parabéns! Você conhece mesmo sobre o React.
+        </h2>
       </>
     );
-  } else if (props.porcentagemPontuacao == 100) {
+  } else if (porcentagemPontuacao === 100) {
     return (
       <>
-        <img className="imgResult" src={ImgResult5} alt="ImgResult5" />
+        <img
+          className="img-resultado"
+          src={ImgResult5}
+          alt="Emblema Radioativo"
+        />
         <h1 className="titulo-resultado">Radioativo</h1>
-        
+        <h2 className="mensagem-resultado"> React rocks!</h2>
       </>
     );
   }
