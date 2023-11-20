635ba4aa4be0da84b25375a7924f93f3f53b69c4
Leonardonps <leonardonpsilva@gmail.com>
2023-11-20 05:55:58 -0300

    Adicionando back-end

---
+++ back-end/react-quiz/src/main/java/com/reactquiz/reactquiz/ReactQuizApplication.java
@@ -0,0 +1,13 @@
+package com.reactquiz.reactquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactQuizApplication.class, args);
+	}
+
+}
